9cbb05bf2c96b4a408d3420726ca8a99410c8665
Zvonimir Juric <zvonimir.juric@outlook.com>
2025-05-17 14:38:09 +0200

    Skappat produkt  tabell

---
+++ data/produkter.sql
@@ -0,0 +1,7 @@
+CREATE TABLE produkter (
+    product.id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product.name TEXT NOT NULL,
+    product.price TEXT NOT NULL,
+    product.brand TEXT NOT NULL
+    
+);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+const { render } = require('ejs');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', 
+  { title: 'Populera produktär', 
+    produkter: produkt
+    });
+  });
 
 module.exports = router;

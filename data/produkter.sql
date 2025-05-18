f9cbad41a4c9c1a901c55dfae1bdca598c7b3565
Zvonimir Juric <zvonimir.juric@outlook.com>
2025-05-18 20:12:54 +0200

    Jag har skappat databasen

--- data/produkter.sql
+++ data/produkter.sql
@@ -1,7 +1,18 @@
 CREATE TABLE produkter (
-    product.id INTEGER PRIMARY KEY AUTOINCREMENT,
-    product.name TEXT NOT NULL,
-    product.price TEXT NOT NULL,
-    product.brand TEXT NOT NULL
-    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price TEXT NOT NULL,
+    brand TEXT NOT NULL
 );
+
+INSERT INTO produkter (name, price, brand) VALUES
+('Svart T-Shirt', '199 SEK', 'Levis'),
+('Svart T-Shirt', '199 SEK', 'Levis'),
+('Svart T-Shirt', '199 SEK', 'Levis'),
+( 'Svart T-Shirt', '199 SEK', 'Levis'),
+('Svart T-Shirt', '199 SEK', 'Levis'),
+('Svart T-Shirt', '199 SEK', 'Levis'),
+('Svart T-Shirt', '199 SEK', 'Levis'),
+('Svart T-Shirt', '199 SEK', 'Levis');
+
+DROP TABLE produkter;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 const { render } = require('ejs');
 var express = require('express');
 var router = express.Router();
+var Database = require('better-sqlite3');
+var db = new Database('./data/Populera-produkter.db', { verbose: console.log });
+//Annan sökväg för databasen;
+//var db = new Database(path.join(__dirname, 'data', 'Populera-produkter.db'), { verbose: console.log });
+
 
 /* GET home page. */
+// Example: Define 'produkt' as an empty array or your actual product data
 router.get('/', function(req, res, next) {
+  //SQL query to fetch all products
+  const sql = 'SELECT * FROM products';
+  //Hämta alla produkter
+  const select = db.prepare(sql);
+  //Skickar SQL kommando till databasen
+  //Hämtar alla produkter
+  const products = select.all();
   res.render('index', 
   { title: 'Populera produktär', 
-    produkter: produkt
+    products: products
     });
   });
 
